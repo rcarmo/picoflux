miniflux/v2

refactor(integration): factorize JSON request construction
Every integration talking to a JSON API hand-rolled the same dance:
json.Marshal the payload, build an *http.Request, set the Content-Type and
User-Agent headers, then run it through NewClientWithOptions. This is
near-identical copy-paste, so any change done to how requests are done had to
be repeated in each one.

This commit introduces a small request builder in internal/http/client and
route every integration through it:

```
client.NewRequestBuilder(endpoint).
	WithMethod(http.MethodPost).
	WithJSON(payload).
	WithHeaders(extraHeaders).
	Do()
```

Centralizing the logic also evens out disparities that had crept in between
integrations:

- private networks are now blocked everywhere, honoring
  INTEGRATION_ALLOW_PRIVATE_NETWORKS; notion previously did not block them;
- request errors are wrapped consistently with %w instead of a mix of %v/%w;
- the per-integration defaultClientTimeout constant is replaced by a single
  shared default in the client package;
- the Content-Type and Miniflux User-Agent headers are always set.

Reviewed-By: gudvinr

internal/http/client/client.go
@@ -4,15 +4,22 @@
 package client // import "miniflux.app/v2/internal/http/client"
 
 import (
+	"bytes"
 	"context"
+	"encoding/json"
 	"errors"
 	"fmt"
+	"io"
 	"net"
 	"net/http"
 	"time"
 
+	"miniflux.app/v2/internal/config"
 	"miniflux.app/v2/internal/urllib"
+	"miniflux.app/v2/internal/version"
 )
+
+const defaultRequestTimeout = 10 * time.Second
 
 // ErrPrivateNetwork is returned when a connection to a private network is blocked.
 var ErrPrivateNetwork = errors.New("client: connection to private network is blocked")
@@ -67,4 +74,87 @@ func NewClientWithOptions(opts Options) *http.Client {
 		Timeout:   opts.Timeout,
 		Transport: transport,
 	}
+}
+
+// requestBuilder builds and executes HTTP requests with the builder pattern.
+type requestBuilder struct {
+	err      error
+	endpoint string
+	method   string
+	body     io.Reader
+	headers  http.Header
+}
+
+// NewRequestBuilder creates a new request builder for the given endpoint.
+func NewRequestBuilder(endpoint string) *requestBuilder {
+	return &requestBuilder{
+		endpoint: endpoint,
+		method:   http.MethodGet,
+		headers:  make(http.Header),
+	}
+}
+
+// WithMethod sets the HTTP method.
+func (r *requestBuilder) WithMethod(method string) *requestBuilder {
+	r.method = method
+	return r
+}
+
+// WithHeader sets a header value.
+func (r *requestBuilder) WithHeader(key, value string) *requestBuilder {
+	r.headers.Set(key, value)
+	return r
+}
+
+// WithJSON marshals payload as JSON, sets the body and Content-Type.
+func (r *requestBuilder) WithJSON(payload any) *requestBuilder {
+	requestBody, err := json.Marshal(payload)
+	if err != nil {
+		r.err = fmt.Errorf("unable to encode request body: %w", err)
+		return r
+	}
+
+	r.body = bytes.NewReader(requestBody)
+	r.headers.Set("Content-Type", "application/json")
+	return r
+}
+
+// Do builds and executes the request.
+//
+// Private networks are blocked unless explicitly allowed through the
+// INTEGRATION_ALLOW_PRIVATE_NETWORKS option.
+func (r *requestBuilder) Do() (*http.Response, error) {
+	if r.err != nil {
+		return nil, r.err
+	}
+
+	// The request is assembled lazily here rather than being stored as a
+	// prebuilt *http.Request in the builder: http.NewRequest inspects the
+	// body's concrete type (e.g. *bytes.Reader) to populate ContentLength and
+	// GetBody. Constructing it only once the body is known yields a correct
+	// Content-Length header and lets the client replay the body on redirects.
+	req, err := http.NewRequest(r.method, r.endpoint, r.body)
+	if err != nil {
+		return nil, fmt.Errorf("unable to create request: %w", err)
+	}
+
+	for key, values := range r.headers {
+		for _, value := range values {
+			req.Header.Add(key, value)
+		}
+	}
+
+	req.Header.Set("User-Agent", "Miniflux/"+version.Version)
+
+	clientOptions := Options{
+		Timeout:              defaultRequestTimeout,
+		BlockPrivateNetworks: !config.Opts.IntegrationAllowPrivateNetworks(),
+	}
+
+	response, err := NewClientWithOptions(clientOptions).Do(req)
+	if err != nil {
+		return nil, fmt.Errorf("unable to send request: %w", err)
+	}
+
+	return response, nil
 }

rcarmo/picoflux

(cherry picked from commit 706a92e700ef760f4df1f486339535fbacb7abd4)

internal/http/client/client.go
@@ -4,15 +4,22 @@
 package client // import "miniflux.app/v2/internal/http/client"
 
 import (
+	"bytes"
 	"context"
+	"encoding/json"
 	"errors"
 	"fmt"
+	"io"
 	"net"
 	"net/http"
 	"time"
 
+	"miniflux.app/v2/internal/config"
 	"miniflux.app/v2/internal/urllib"
+	"miniflux.app/v2/internal/version"
 )
+
+const defaultRequestTimeout = 10 * time.Second
 
 // ErrPrivateNetwork is returned when a connection to a private network is blocked.
 var ErrPrivateNetwork = errors.New("client: connection to private network is blocked")
@@ -67,4 +74,94 @@ func NewClientWithOptions(opts Options) *http.Client {
 		Timeout:   opts.Timeout,
 		Transport: transport,
 	}
+}
+
+// requestBuilder builds and executes HTTP requests with the builder pattern.
+type requestBuilder struct {
+	err      error
+	endpoint string
+	method   string
+	body     io.Reader
+	headers  http.Header
+}
+
+// NewRequestBuilder creates a new request builder for the given endpoint.
+func NewRequestBuilder(endpoint string) *requestBuilder {
+	return &requestBuilder{
+		endpoint: endpoint,
+		method:   http.MethodGet,
+		headers:  make(http.Header),
+	}
+}
+
+// WithMethod sets the HTTP method.
+func (r *requestBuilder) WithMethod(method string) *requestBuilder {
+	r.method = method
+	return r
+}
+
+// WithHeader sets a header value.
+func (r *requestBuilder) WithHeader(key, value string) *requestBuilder {
+	r.headers.Set(key, value)
+	return r
+}
+
+// WithJSON marshals payload as JSON, sets the body and Content-Type.
+func (r *requestBuilder) WithJSON(payload any) *requestBuilder {
+	requestBody, err := json.Marshal(payload)
+	if err != nil {
+		r.err = fmt.Errorf("unable to encode request body: %w", err)
+		return r
+	}
+
+	return r.WithJSONBody(requestBody)
+}
+
+// WithJSONBody sets an already-marshaled JSON body and the Content-Type.
+// It is useful when the caller needs the encoded payload for another
+// purpose (e.g. computing a signature) to avoid marshaling it twice.
+func (r *requestBuilder) WithJSONBody(body []byte) *requestBuilder {
+	r.body = bytes.NewReader(body)
+	r.headers.Set("Content-Type", "application/json")
+	return r
+}
+
+// Do builds and executes the request.
+//
+// Private networks are blocked unless explicitly allowed through the
+// INTEGRATION_ALLOW_PRIVATE_NETWORKS option.
+func (r *requestBuilder) Do() (*http.Response, error) {
+	if r.err != nil {
+		return nil, r.err
+	}
+
+	// The request is assembled lazily here rather than being stored as a
+	// prebuilt *http.Request in the builder: http.NewRequest inspects the
+	// body's concrete type (e.g. *bytes.Reader) to populate ContentLength and
+	// GetBody. Constructing it only once the body is known yields a correct
+	// Content-Length header and lets the client replay the body on redirects.
+	req, err := http.NewRequest(r.method, r.endpoint, r.body)
+	if err != nil {
+		return nil, fmt.Errorf("unable to create request: %w", err)
+	}
+
+	for key, values := range r.headers {
+		for _, value := range values {
+			req.Header.Add(key, value)
+		}
+	}
+
+	req.Header.Set("User-Agent", "Miniflux/"+version.Version)
+
+	clientOptions := Options{
+		Timeout:              defaultRequestTimeout,
+		BlockPrivateNetworks: !config.Opts.IntegrationAllowPrivateNetworks(),
+	}
+
+	response, err := NewClientWithOptions(clientOptions).Do(req)
+	if err != nil {
+		return nil, fmt.Errorf("unable to send request: %w", err)
+	}
+
+	return response, nil
 }